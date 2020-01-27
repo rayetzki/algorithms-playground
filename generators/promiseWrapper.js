a8ed9839722610ff7f9130bf2ad880020994bb59
ILRA1 <illia.raietskyi@mcpk.tech>
2020-01-27 11:34:53 +0200

    Refactored generator function

--- generators/promiseWrapper.js
+++ generators/promiseWrapper.js
@@ -1,28 +1,25 @@
 const fetch = require('node-fetch')
 
-async function* handlePromisesGenerator(promiseArr, fetch) {
-    let handledPromise
-    const handledPromiseCollection = []
-
-    for (let promise of promiseArr) {
-        handledPromise = await fetch(promise)
-        if (handledPromise.status === 200) {
-            handledPromiseCollection.push(yield { response: await handledPromise.json(), status: 'resolved' })
+async function* handlePromisesGenerator(arrayOfLinks) {
+    for (let promise of arrayOfLinks) {
+        const promiseResult = await promise
+        if (promiseResult.status === 200) {
+            yield { response: await promiseResult.json(), status: 'resolved' }
         } else {
-            handledPromiseCollection.push(yield { response: await handledPromise.json(), status: 'rejected' })
+            yield { response: await promiseResult.json(), status: 'rejected' }
         }
     }
 }
 
-const promiseCollection = []
+const arrayOfLinks = []
 
-for (let i = 1; i <= 9; i++) {
-    promiseCollection.push(`https://jsonplaceholder.typicode.com/todos/${i}`)
+for (let i = 1; i <= 100; i++) {
+    arrayOfLinks.push(fetch(`https://jsonplaceholder.typicode.com/todos/${i}`))
 }
 
 (async () => {
     try {
-        for await (let promise of handlePromisesGenerator(promiseCollection, fetch)) {
+        for await (let promise of handlePromisesGenerator(arrayOfLinks)) {
             console.log(promise)
         }
     } catch(error) {
