4134b8749fac8b6dcf06fc2ed0ae739698f4b113
rayetzki <rayetzkiillya@gmail.com>
2020-12-11 12:52:45 +0200

    Better error handling on promise wrapper

--- generators/promiseWrapper.js
+++ generators/promiseWrapper.js
@@ -1,28 +1,33 @@
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 
 async function* handlePromisesGenerator(arrayOfLinks) {
-    for (let promise of arrayOfLinks) {
-        const promiseResult = await promise
-        if (promiseResult.status === 200) {
-            yield { response: await promiseResult.json(), status: 'resolved' }
-        } else {
-            yield { response: await promiseResult.json(), status: 'rejected' }
-        }
+  for (let promise of arrayOfLinks) {
+    try {
+      const response = await promise;
+      if (response) {
+        yield {
+          response: response.json(),
+          status: "resolved",
+        };
+      }
+    } catch (error) {
+      yield { response: error.message, status: "rejected" };
     }
+  }
 }
 
-const arrayOfLinks = []
+const arrayOfLinks = [];
 
 for (let i = 1; i <= 100; i++) {
-    arrayOfLinks.push(fetch(`https://jsonplaceholder.typicode.com/todos/${i}`))
+  arrayOfLinks.push(fetch(`https://jsonplaceholder.typicode.com/todos/${i}`));
 }
 
 (async () => {
-    try {
-        for await (let promise of handlePromisesGenerator(arrayOfLinks)) {
-            console.log(promise)
-        }
-    } catch(error) {
-        throw new Error(error)
+  try {
+    for await (let promise of handlePromisesGenerator(arrayOfLinks)) {
+      console.log(promise);
     }
-})()
+  } catch (error) {
+    throw new Error(error);
+  }
+})();
