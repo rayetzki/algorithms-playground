9f350fbbac26ee00a7af4b487003bb21ad5c204f
Illia Rayetzki <rayetzkiillya@gmail.com>
2023-01-11 14:58:55 +0200

    Set covering approximation algorithm

---
+++ algorithms/set-covering.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+states_needed = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
+stations = {}
+stations["kone"]   = set(["id", "nv", "ut"])
+stations["ktwo"]   = set(["wa", "id", "mt"])
+stations["kthree"] = set(["or", "nv", "ca"])
+stations["kfour"]  = set(["nv", "ut"])
+stations["kfive"]  = set(["ca", "az"])
+
+final_stations = set()
+
+def my_set_covering(states_needed, stations):
+  final_stations = set()
+  while states_needed:
+    best_station = None
+    states_covered = set()
+    for station, states in stations.items():
+      covered = states_needed & states
+      if len(covered) > len(states_covered) and station not in final_stations:
+        best_station = station
+        states_covered = covered
+    if best_station is not None:
+      states_needed -= states_covered
+      final_stations.add(best_station)
+      stations.pop(best_station)
+    else:
+      return None
+  return list(final_stations)
+
+print(my_set_covering(states_needed, stations))
